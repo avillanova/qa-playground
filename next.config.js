74655b18278e52cd386e14bb5399845fc12369e5
Alex villanova <avillanova@MBP-de-Alex.hitronhub.home>
2024-01-02 18:07:44 -0700

    Fix CORS issue

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'https://qa-playground.vercel.app'
+            value: process.env.NEXT_PUBLIC_VERCEL_URL
           },
           {
             key: 'Access-Control-Allow-Methods',
