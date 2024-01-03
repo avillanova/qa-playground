49fc64fd4a4945ee574b14554e0a3e961683d9e3
Alex villanova <avillanova@MBP-de-Alex.hitronhub.home>
2024-01-02 17:43:23 -0700

    add NEXT_PUBLIC_VERCEL_URL

--- src/services/AssessmentService.ts
+++ src/services/AssessmentService.ts
@@ -3,7 +3,7 @@ import { AssessmentListType, AssessmentType } from '@/types/AssessmentType';
 export function getAssessment(assessmentId: string): Promise<any> {
   console.log('getAssessment', assessmentId);
   const assessment: Promise<AssessmentType> = fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/assessments/${assessmentId}`,
+    `${getBaseUrl()}/api/assessments/${assessmentId}`,
     {
       next: { revalidate: 10 }
     }
@@ -13,12 +13,19 @@ export function getAssessment(assessmentId: string): Promise<any> {
 
 export function getAssessmentList(): Promise<AssessmentListType[]> {
   console.log('get List');
-  console.log(`Base Url: ${process.env.NEXT_PUBLIC_VERCEL_URL}`);
   const assessments: Promise<AssessmentListType[]> = fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/assessments`,
+    `${getBaseUrl()}/api/assessments`,
     {
       next: { revalidate: 10 }
     }
   ).then((res) => res.json());
   return assessments;
+}
+
+function getBaseUrl() {
+  console.log(`Base Url: ${process.env.NEXT_PUBLIC_VERCEL_URL}`);
+  if (process.env.NEXT_PUBLIC_VERCEL_URL?.includes('localhost')) {
+    return `http://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  }
+  return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 }
