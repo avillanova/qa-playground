ecf8e2ceeecca75c6dc0a9013f8fa01517b2b117
Alex villanova <avillanova@MBP-de-Alex.hitronhub.home>
2024-01-06 10:48:19 -0700

    com barra

--- next.config.js
+++ next.config.js
@@ -1,22 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   async headers() {
     return [
       {
-        source: '/api/(.*)',
+        // matching all API routes
+        source: '/api/:path*',
         headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://www.qa-playground.vercel.app'
-          },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           {
             key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS'
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
           }
         ]
       }
     ];
   }
 };
-
-module.exports = nextConfig;
