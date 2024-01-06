5cd819dfd2e64d5f105ff839a113157da7ea21fc
Alex villanova <avillanova@MBP-de-Alex.hitronhub.home>
2024-01-06 11:15:58 -0700

    update node version

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: '*'
+            value: 'https://www.alejandroperez.dev'
           },
           {
             key: 'Access-Control-Allow-Methods',
